a7da6530902dcb67f5c888fb68d43b9c56666df8
Aditi Singh <787aditi@gmail.com>
2017-02-12 10:39:38 -0600

    Pruning operation works

--- main.py
+++ main.py
@@ -2,8 +2,33 @@ import pygtrie as trie
 import itertools
 import string
 import numpy as np
+import sys
 
-filename='transactionDB2.txt'
+def get_support_vals(trie.StringTrie t,list x, int sz, int node):
+	support_vals=np.zeros((node,1))
+	for transaction in x: #for every transaction
+		curr=list(itertools.combinations(transaction,sz))
+		for i in range(len(curr)):
+			key=str(curr[i][0])
+			for j in range(1,len(curr[i])):
+				key=key+'/'+str(curr[i][j])
+			#constructed key
+			if(t.has_key(key)):	#add it
+				node_val=t.__getitem__(key)
+				support_vals[node_val]=support_vals[node_val]+1
+	return support_vals
+
+
+total=len(sys.argv)
+#Check the number of commandline arguments
+if(total!=3):
+	print('Invalid number of arguments, try again.')
+	sys.exit()
+
+minsup_count=int(sys.argv[0])
+k=int(sys.argv[1])
+
+filename=str(sys.argv[2])#'transactionDB2.txt'
 
 f=open(filename,'r')
 transaction_DB=[line.split(' ') for line in f.readlines()]
@@ -13,30 +38,27 @@ for i in range(len(transaction_DB)):
 
 num_transactions=len(transaction_DB)
 
-minsup_count=5
-k=7
-
 if(k>len(max(transaction_DB,key=len))):
 	print('Value of k cannot be larger than longest transaction')
+	sys.exit()
 
-words=open(filename).read().split()
+words=np.unique(open(filename).read().split())	#contains all words unique
 
-t=trie.StringTrie(enable=True)
+t=trie.StringTrie()
 
-#Create a trie with it
-itemset_1=[]
-idx1=0
+#Create first trie 
+count=0
 for x in words:
-	if x not in itemset_1:
-		itemset_1.append(x)
-		t[x]=idx1
-		idx1=idx1+1
+	t[x]=count
+	count=count+1
 
+#Get support values
+support_vals=get_support_vals(t,transaction_DB)
 
 #for every transaction find subsets of size 1
 sz=1
-support_vals=np.zeros((len(t.keys()),1))
-
+support_vals=np.zeros((max(t.values()),1))
+#fix
 for i in range(len(transaction_DB)):
 	curr=list(itertools.combinations(transaction_DB[i],sz))
 	for j in range(len(curr)):
