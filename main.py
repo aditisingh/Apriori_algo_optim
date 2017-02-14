599afd5b583cb55b07326c69935cf1884050d34d
Aditi Singh <787aditi@gmail.com>
2017-02-13 18:34:31 -0600

    Converting string to python, saves time

--- main.py
+++ main.py
@@ -7,22 +7,24 @@ import numpy as np
 import sys
 import time
 
-def get_support_vals(t, x, node):
-	support_vals=np.zeros((1,node))
-	for sz in range(1, len(max(x,key=len))+1):
-		for transaction in x: #for every transaction
-			curr=list(itertools.combinations(transaction,sz))
-			for i in range(len(curr)):
-				key=str(curr[i][0])
-				for j in range(1,len(curr[i])):
-					key=key+'/'+str(curr[i][j])
-				#constructed key
-				if(t.has_key(key)):	#add it
-					node_val=t.__getitem__(key)
-					support_vals[0][node_val]=support_vals[0][node_val]+1
+def get_support_vals(t, x, node, sz):
+	support_vals=np.zeros((1,node))	
+	for transaction in x: #for every transaction
+		curr=list(itertools.combinations(transaction,sz))
+		for i in range(len(curr)):
+			key=str(word_index[curr[i][0]])
+			for j in range(1,len(curr[i])):
+				key=key+'/'+str(word_index[curr[i][j]])
+			#constructed key
+			print(t)
+			print(key)
+			print('\n')
+			if(t.has_key(key)):	#add it
+				node_val=t.__getitem__(key)
+				support_vals[0][node_val]=support_vals[0][node_val]+1
 	for key in t.keys():
-		idx=t[key]
-		t[key]=support_vals[0][idx]
+			idx=t[key]
+			t[key]=support_vals[0][idx]
 	return t;
 
 def pruning_trie(t,minsup_count):
@@ -61,21 +63,57 @@ if(k>len(max(transaction_DB,key=len))):
 words=np.unique(open(filename).read().split())	#contains all words unique
 words.sort()
 words=words.tolist()
+word_index=range(len(words)) #conversion of words to int
+
+for i in range(len(transaction_DB)):
+	for j in range(len(transaction_DB[i])):
+		transaction_DB[i][j]=word_index[words.index(transaction_DB[i][j])]
+	transaction_DB[i].sort() #sorting every transaction_DB
+
+print ('Argument reading time: ' + str(time.clock() - start)+ ' seconds')
+pt_1= time.clock()
 
 t=trie.StringTrie()
 
-#Create first trie 
 count=0
-for x in words:
-	t[x]=count
+
+for x in word_index:
+	t[str(x)]=count
 	count=count+1
+
+f_o=open(out,'r+')
+for sz in range(1, len(max(transaction_DB,key=len))):
+	t=get_support_vals(t,transaction_DB,1+max(t.values()),sz)
+	t=pruning_trie(t,minsup_count)
+	for i in range(len(t.keys())):
+		key=t.keys()[i]
+		value=int(t.__getitem__(key))
+		f_o.write(str(words[int(key)])+'\t ('+str(value)+')')
+	j=0
+	element=t.keys()[j].split('/')
+	idx=word_index.index(int(element[-1]))
+	gr_idx=word_index[idx+1:]
+	for n in range(len(gr_idx)):
+		element_2=str(t.keys()[i])+'/'+str(gr_idx[n])
+		if(~t.has_key(element_2)):
+			t[element_2]=count+1
+		count=count+1
+f_o.close()
+
+for i in range(len(t.keys())):
+	key=t.keys()[i]
+	value=int(t.__getitem__(key))
+	f_o.write(str(words[int(key)])+'\t ('+str(value)+')')
+
+
+
 
 #Candidate generation
 i=0
 while(i<len(t.keys())):
 	element=t.keys()[i].split('/')
-	idx=words.index(element[-1])
-	gr_idx=words[idx+1:]
+	idx=word_index.index(int(element[-1]))
+	gr_idx=word_index[idx+1:]
 	for n in range(len(gr_idx)):
 		element_2=str(t.keys()[i])+'/'+str(gr_idx[n])
 		if(~t.has_key(element_2)):
@@ -83,13 +121,21 @@ while(i<len(t.keys())):
 		count=count+1
 	i=i+1
 
+print ('Candidate generation time: ' + str(time.clock() - pt_1)+ ' seconds')
+pt_2= time.clock() 
+
 # all candidates ready
 
 #Next, we get all the support count for each candidate
-t=get_support_vals(t,transaction_DB,1+max(t.values()))
+t=get_support_vals(t,transaction_DB,1+max(t.values()),k,1)
+
+print ('Support value calculation time: ' + str(time.clock() - pt_2)+ ' seconds')
+pt_3= time.clock()
 
 #pruning operation
 t=pruning_trie(t,minsup_count)
+
+print ('Pruning operation time: ' + str(time.clock() - pt_3)+ ' seconds')
 
 print(t)
 print ('Total execution time: ' + str(time.clock() - start)+ ' seconds')
