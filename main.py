68aea67e5b69617cbfed8cdb573ef8ac07c09cb8
Aditi Singh <787aditi@gmail.com>
2017-02-18 01:49:09 -0600

    Replaced Trie by Dictionary

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import sys
 import time
 import cProfile
 
-def get_support_vals(t, x, node, sz): #not working for >=2 number of itemsets
+def get_support_vals(dict, x, node, sz): #not working for >=2 number of itemsets
 	support_vals=np.zeros((1,node))	
 	p1=time.clock()
 	for transaction in x: #for every transaction
@@ -16,25 +16,18 @@ def get_support_vals(t, x, node, sz): #not working for >=2 number of itemsets
 		for curr in comb:
 			key='/'.join(str(x) for x in (curr))
 			#constructed key
-			if(t.has_key(key)):	#add it
-				node_val=t.__getitem__(key)
+			if(key in dict.keys()):	#add it
+				node_val=dict[str(key)]
 				support_vals[0][node_val]=support_vals[0][node_val]+1
 	print('Part1 time: ' + str(time.clock() - p1)+ ' seconds')
 	p2=time.clock()
-	for key in t.keys():
+	for key in dict:
 		if(key.count('/')==sz-1):
-			idx=t[key]
-			t[key]=support_vals[0][idx]
+			idx=dict[key] 
+			dict[key]=support_vals[0][idx]
 			# print(t[key])
 	print('Part2 time: ' + str(time.clock() - p2)+ ' seconds')
-	return t;
-
-def pruning_trie(t,minsup_count,sz):
-	for word in list(t):
-		if word.count('/')==(sz-1):
-			if t[word]<minsup_count:
-				del t[word]
-	return t;
+	return dict;
 
 start = time.clock()
 
@@ -78,28 +71,36 @@ pt_1= time.clock()
 
 sz=1
 print('Size: '+str(sz))
-t=trie.StringTrie()
-
-count=0
-
-for x in word_index:
-	t[str(x)]=count
+dict={}
+count=1
+for i in word_index:   
+	dict[str(i)]=count
 	count=count+1
 
 print('Candidate generation done in '+ str(time.clock() - pt_1)+ ' seconds')
 f_o=open(out,'r+')
 pt_2= time.clock()
-t=get_support_vals(t,transaction_DB,1+max(t.values()),sz) 
+# cProfile.run(get_support_vals(get_support_vals(dict,transaction_DB,count,sz))
+dict=get_support_vals(dict,transaction_DB,count,sz) 
 print('Support values generation done in '+ str(time.clock() - pt_2)+ ' seconds')
 pt_3=time.clock()
-t=pruning_trie(t,minsup_count,sz)
+# cProfile.run(dict,minsup_count,sz)
+
+#pruning
+dict1={}
+for key in dict:
+		if key.count('/')==(sz-1):
+			if dict[key]>=minsup_count:
+				dict1[key]=dict[key]
+dict=dict1
+
 print('Pruning done in '+ str(time.clock() - pt_3)+ ' seconds')
 pt_4=time.clock()
 if(k<=sz):
-	l=list(t)
+	l=list(dict)
 	l1=[ll for ll in l if ll.count('/')==sz-1]
 	for key in l1:
-		value=int(t.__getitem__(key))
+		value=dict[key]
 		list_element=key.split('/')
 		list_element.sort(key=int)
 		for element in list_element:
@@ -112,7 +113,7 @@ for sz in range(2, 1+len(max(transaction_DB,key=len))):
 	print('Size: '+str(sz))
 	pt_5=time.clock()
 	add1=0
-	l=list(t)
+	l=list(dict)
 	l1=[ll for ll in l if ll.count('/')==0]
 	l2=[ll for ll in l if ll.count('/')==sz-2]
 	if(len(l2)==0):
@@ -123,11 +124,12 @@ for sz in range(2, 1+len(max(transaction_DB,key=len))):
 		element='/'.join(c)
 		if(element.count('/')==sz-1):
 			ll=element.split('/')
+			ll=list(set(ll))
 			ll.sort(key=int)
 			element='/'.join(ll)
 			result = list(( ll.count(i)) for i in ll)
 			if(sum(result)-len(result)==0):
-				t[element]=count+1
+				dict[element]=count+1
 				add1=add1+1
 				count=count+1
 	# print(t)
@@ -137,28 +139,34 @@ for sz in range(2, 1+len(max(transaction_DB,key=len))):
 	# print('\n')
 	print('Candidate generation done '+ str(time.clock() - pt_5)+ ' seconds')
 	pt_2= time.clock()
-	cProfile.run(get_support_vals(t,transaction_DB,1+max(t.values()),sz))
-	t=get_support_vals(t,transaction_DB,1+max(t.values()),sz) # prune outside
+	# cProfile.run(get_support_vals(get_support_vals(dict,transaction_DB,count,sz))
+	dict=get_support_vals(dict,transaction_DB,count,sz) # prune outside
 	
 	print('Support values generation done in '+ str(time.clock() - pt_2)+ ' seconds')
 	# print(t)
 	pt_3=time.clock()
-	cProfile.run(t,minsup_count,sz)
-	t=pruning_trie(t,minsup_count,sz)
+	# cProfile.run(d,minsup_count,sz)
+	#pruning
+	dict1={}
+	for key in dict:
+			if key.count('/')==(sz-1):
+				if dict[key]>=minsup_count:
+					dict1[key]=dict[key]
+	dict=dict1
 	print('Pruning done in '+ str(time.clock() - pt_3)+ ' seconds')
 	# print(t)
 	pt_4=time.clock()
 	if(k<=sz):
-		l=list(t)
+		l=list(dict)
 		l1=[ll for ll in l if ll.count('/')==sz-1]
 		for key in l1:
-			value=int(t.__getitem__(key))
+			value=dict[key]
 			list_element=key.split('/')
 			list_element.sort(key=int)
 			for element in list_element:
 				f_o.write(str(words[int(element)])+' ')
 			f_o.write('\t ('+str(value)+') \n')
-		print('Writing done in '+ str(time.clock() - pt_4)+ ' seconds')
+	print('Writing done in '+ str(time.clock() - pt_4)+ ' seconds')
 	print('\n')
 	# j=0
 	
